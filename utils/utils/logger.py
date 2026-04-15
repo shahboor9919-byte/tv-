f27adc274768784e2d9fb2756c8944869678e86b
shahboor9919-byte <shahboor9919@gmail.com>
2026-04-15 09:40:29 +0200

    Update logger.py

--- utils/utils/logger.py
+++ utils/utils/logger.py
@@ -1,31 +1,23 @@
 import logging
-import colorlog
-import sys
 
-def setup_logger(name: str = "iptv_aggregator", level: int = logging.INFO) -> logging.Logger:
-    """Configure structured colored logging."""
+def setup_logger(name="iptv_engine", level=logging.INFO):
     logger = logging.getLogger(name)
+
     if logger.handlers:
         return logger
-    
+
     logger.setLevel(level)
-    
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(level)
-    
-    formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'red,bg_white',
-        }
+
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
     )
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
     return logger
 
 logger = setup_logger()
