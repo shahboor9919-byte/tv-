2be8de119e1647f34a0b8c69d094adc13075bd2c
shahboor9919-byte <shahboor9919@gmail.com>
2026-04-15 10:49:03 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+def setup_logger(name="iptv_engine", level=logging.INFO):
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(level)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
